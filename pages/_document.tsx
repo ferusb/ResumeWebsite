228f51ffd03ccea99550bb48a64ea2f363123644
Brian Ferus <btferus@gmail.com>
2025-10-19 11:40:00 -0700

    Convert pages to TypeScript with proper typing
    - Migrate _app.jsx to _app.tsx with AppProps
    - Migrate _document.jsx to _document.tsx with DocumentContext
    - Add explicit type annotations and event handler typing

---
+++ pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
+import type { ReactElement } from 'react';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
+  }
+
+  render(): ReactElement {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
